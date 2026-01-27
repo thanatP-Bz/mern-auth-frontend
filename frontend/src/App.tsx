bd23627deb936953b8bf2a6a383345f5492e9c95
Thanat <bztzn512@gmail.com>
2026-01-27 14:38:51 -0500

    update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -55,7 +55,7 @@ const router = createBrowserRouter([
       { path: "verify-email", element: <VerifyEmail /> },
       { path: "forget-password", element: <ForgetPassword /> },
       { path: "reset-password/:token", element: <ResetPassword /> },
-      { path: "/auth/callback", element: <OAuthCallback /> },
+      { path: "/oauth/callback", element: <OAuthCallback /> },
     ],
   },
 
